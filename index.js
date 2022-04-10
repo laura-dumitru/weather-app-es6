de60c916551e439b77864e9c6bb7d934295a5ac4
Laura Dumitru <lauradumitru88@icloud.com>
2022-04-10 20:26:43 +0100

    Added API and location

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const apiKey = "e3dda97cfe9d9fc23a4b5fa7130913b1";
 
 const temperatureElement = document.querySelector("#weather-temperature");
+
+const iconElement = document.querySelector("#icon lottie-player");
+
+const locationElement = document.querySelector("#location");
 const cityElement = document.querySelector("#city");
+
 const descriptionElement = document.querySelector("#description");
 const humidityElement = document.querySelector("#humidity");
 
-// const displayTemperature = (response) => {
-function displayTemperature(response) {
-  console.log(response.data.main.temp);
-
+// const displayWeather = (response) => {
+function displayWeather(response) {
   let unit = "C";
 
   temperatureElement.innerHTML = `${Math.round(
     response.data.main.temp
   )}°${unit}`;
+
+  let iconCode = response.data.weather[0].icon;
+  //console.log(iconCode);
+  iconElement.setAttribute("src", `img/${iconCode}.json`);
+  //console.log(iconElement);
 
   cityElement.innerHTML = response.data.name;
 
@@ -22,13 +30,21 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
 }
 
-let city = "New York";
+//let showPosition = (position) => {
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
 
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let city = "Budapest";
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-//console.log(apiURL);
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+}
 
-axios.get(apiURL).then(displayTemperature);
+navigator.geolocation.getCurrentPosition(showPosition);
+
+/*
 
 // const displayFahrenheitTemperature = (event) => {
 function displayFahrenheitTemperature(event) {
@@ -37,6 +53,4 @@ function displayFahrenheitTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
-const fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", displayFahrenheitTemperature);
+*/
