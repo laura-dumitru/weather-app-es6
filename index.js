1e2b672c59dac076029ba0f368568bd8201f6b9b
Laura Dumitru <lauradumitru88@icloud.com>
2022-04-11 16:33:02 +0100

    Added fahrenheit temperature on tile click

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const apiKey = "e3dda97cfe9d9fc23a4b5fa7130913b1";
 
-const temperatureElement = document.querySelector("#weather-temperature");
+const temperatureTileElement = document.querySelector("#temperature");
+const temperatureElement = document.querySelector("#temperature p");
 
 const iconElement = document.querySelector("#icon lottie-player");
 
@@ -10,17 +11,20 @@ const cityElement = document.querySelector("#city");
 const descriptionElement = document.querySelector("#description");
 const humidityElement = document.querySelector("#humidity");
 
+const convertTemperature = (celcius) => (celcius * 9) / 5 + 32;
+
 //function displayWeather(response) {
 const displayWeather = (response) => {
-  let unit = "C";
+  let temperature;
+  if (temperatureElement.innerHTML.endsWith("C")) {
+    const fahrenheitTemperature = convertTemperature(response.data.main.temp);
+    temperature = `${Math.round(fahrenheitTemperature)}°F`;
+  } else {
+    temperature = `${Math.round(response.data.main.temp)}°C`;
+  }
+  temperatureElement.innerHTML = temperature;
 
-  temperatureElement.innerHTML = `${Math.round(
-    response.data.main.temp
-  )}°${unit}`;
-
-  let iconCode = response.data.weather[0].icon;
-
-  iconElement.load(`img/${iconCode}.json`);
+  iconElement.load(`img/${response.data.weather[0].icon}.json`);
 
   cityElement.innerHTML = response.data.name;
 
@@ -35,21 +39,17 @@ const showPosition = (position) => {
   let longitude = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
 
-  //let city = "Tokyo";
+  //let city = "Valencia";
   //apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
 };
 
+const changeTemperature = (event) => {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+};
+
 navigator.geolocation.getCurrentPosition(showPosition);
 
-/*
-
-// const displayFahrenheitTemperature = (event) => {
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (13 * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-*/
+temperatureTileElement.addEventListener("click", changeTemperature);
